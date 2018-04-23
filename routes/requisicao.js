188f1d1cd9fa17ab997b498dbc18280b029b81c4
rodrigo <rodrigopds73@gmail.com>
2018-04-23 17:38:11 -0300

    Revert "Merge pull request #1 from LuizASSilveira/front"
    This reverts commit 88209983cee782e9ee19f9fe735db0f86711010f, reversing
    changes made to 37522a7499431614eae436d48bc0f746f2cdd036.
    
    Revertendo Merge do Luiz

--- routes/requisicao.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router  = express.Router();
-var models  = require("../models");
-
-router.get("/", function(req, res) {
-	res.render("requisicao")
-});
-module.exports = router;
