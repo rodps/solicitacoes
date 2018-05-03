f7376b05872e311c7bc5cf99e7db2ca67fa73a32
doouglasabreu <douglasabreu97@gmail.com>
2018-05-03 17:56:51 -0300

    excluindo models

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use("/produtos", produtosRouter);
 //Cria o banco de dados
 //{force:true} Drop tables se ja existirem
 models.sequelize
-  .sync()
+  .sync({force:true})
   .then(() => {
     console.log("Nice! Database looks fine");
     app.listen(3000, function(err) {
