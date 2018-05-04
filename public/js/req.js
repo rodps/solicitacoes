8f086189d78601492addc2050dc90fb5605a12c2
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-05-04 16:46:59 -0300

    modal - erros

--- public/js/req.js
+++ public/js/req.js
@@ -1,58 +1,66 @@
-
 var listRequisicao = []
 var idSolicitacao
-
+var statusSolicitacao
 var solicitacaoTr = document.createElement("tr");
 var xhr = new XMLHttpRequest();
+var btdCarregar
 
-xhr.open("GET", "http://localhost:3000/requisicoes/listar/solicitacoes"); //tipo de requisição + end.
-
+xhr.open("GET", "https://raw.githubusercontent.com/LuizASSilveira/pi-almoxarifado/master/teste.json"); //tipo de requisição + end.
 xhr.addEventListener("load", function(){
     var sol = JSON.parse(xhr.responseText);
     sol.forEach(function(solicitacao) {
-        addSolicitacaoNaTabela(solicitacao);
+    addSolicitacaoNaTabela(solicitacao);
+    
     });
 })
-
 xhr.send();
 
 var tabela = document.querySelector("table");
 tabela.addEventListener("click", function(event){  
-    idSolicitacao = event.target.parentNode.lastChild.textContent
+    row = event.target.parentNode
+    idSolicitacao = row.lastChild.textContent
+    //statusSolicitacao = getStatus(event) //  usar no deserto
+
     if(!listRequisicao.includes(idSolicitacao)){
-        listRequisicao.push(idSolicitacao)    
+        listRequisicao.push(idSolicitacao)
+        row.classList.add("solicitacaoSelecionada")
     }
+    
     else{
-        listRequisicao.pop(idSolicitacao)    
+        listRequisicao.pop(idSolicitacao)
+        row.classList.remove("solicitacaoSelecionada")    
     }
+    
     console.log(listRequisicao)
     //setTimeout(function(){
     //    event.target.parentNode.remove(); //pega campo do duplo click e elimina o pai , fazendo assim apagar a linha
     //},300);
 });
 
-
-var btdCarregar = document.querySelector("#newRequisicao");
-
-    
+btdCarregar = document.querySelector("#saveRequisicao");
     btdCarregar.addEventListener("click",function(){
-        var json = JSON.stringify({"solicitacoes" : listRequisicao});
-        console.log(json)
-        
-        var ajax = new XMLHttpRequest()
-        ajax.open("POST", "http://localhost:3000/requisicoes/criar/requisicoes", true)
-        ajax.setRequestHeader('Content-type','application/json; charset=utf-8');
-         ajax.send(json)
+        if(listRequisicao.length != 0){
+            console.log("okok")
+            var json = JSON.stringify({"solicitacoes" : listRequisicao});
+            var ajax = new XMLHttpRequest()
+            ajax.open("POST", "http://localhost:3000/requisicoes/criar/requisicoes", true)
+            ajax.setRequestHeader('Content-type','application/json; charset=utf-8');
+            ajax.send(json)
+            window.location.reload()
+        }
+        else{
+            document.getElementById("error").style.display = "block";
+            }
 
 })
-
-
 
 ///////////////////////////
 function addSolicitacaoNaTabela(solicitacao){
     var solicitacaoTr = montaTr(solicitacao);
     var tabela = document.querySelector("#tabela-solicitacao");
+    
     tabela.appendChild(solicitacaoTr);
+    return
 }
 function montaTr(solicitacao){
     var solicitacaoTr = document.createElement("tr");
@@ -73,4 +81,11 @@ function montaTd(dado,classe){
     td.textContent = dado;
     td.classList.add(classe);
     return td;
+}
+
+function getStatus(event){
+    if(event.childNodes[2].textContent == "ABERTO"){
+        return true
+    }
+    return false
 }
